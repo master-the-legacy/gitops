5f2c23529599f491988492cc0fd819d855fd5926
Benhur A. Silva <benhur.sre@gmail.com>
2023-08-20 12:15:14 -0300

    feat(infrastructure): created ECR repo

---
+++ infrastructure/ecr.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "legacy-ecr-repo" {
+  name                 = "legacy-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.tags
+}
