7c53a325109166d7ba34cb549fb70d37e9c18bf8
carlosgit2016 <carlosggflor@gmail.com>
2023-08-31 01:36:07 -0400

    New subnets to EKS cluster

---
+++ infrastructure/scaffold/eks.tf
@@ -0,0 +1,3 @@
+# EKS features https://aws.amazon.com/eks/features/
+# AWS will use commercially reasonable efforts to make the Kubernetes endpoint for an Amazon EKS Cluster available with a Monthly Uptime Percentage of at least 99.95%
+# EKS keeps one single tenant control plane per cluster and make sure its high availability
