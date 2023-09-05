58ba7b6eca74c75b1a0fb481dfffe6108d59f642
carlosgit2016 <carlosggflor@gmail.com>
2023-09-05 01:29:50 -0400

    MTL-18 Adjusted subnets

--- infrastructure/scaffold/eks.tf
+++ infrastructure/scaffold/eks.tf
@@ -3,7 +3,7 @@
 
 
 ### EKS Control Plane ###
-resource "aws_eks_cluster" "eks_cluster" {
+resource "aws_eks_cluster" "eks" {
   name     = "${terraform.workspace}-master-cluster" # The name must be unique within the AWS Region and AWS account
   role_arn = aws_iam_role.legacy_eks_role.arn
 
@@ -25,7 +25,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 
   vpc_config {
-    subnet_ids             = aws_subnet.eks_subnets[*].id # You can't change which subnets you want to use after cluster creation.
+    subnet_ids             = [ for az in local.eks_availability_zones : aws_subnet.eks_subnets[az].id] # You can't change which subnets you want to use after cluster creation.
     endpoint_public_access = true                         # Enable EKS public API server endpoint
     public_access_cidrs    = ["0.0.0.0/0"]                # EKS API server endpoint source IP, could be used to allow only VPN
     # security_group_ids = [] TODO: make the cluster private
@@ -34,25 +34,40 @@ resource "aws_eks_cluster" "eks_cluster" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_ecr_repository.legacy-ecr-repo,
     #aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController
   ]
 
-  version = "1.29" # no upgrades will occur except those automatically triggered by EKS
+  # https://kubernetes.io/releases/
+  # Supported versions by EKS https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
+  version = "1.27" # no upgrades will occur except those automatically triggered by EKS
 
   tags = {
     product = "master-the-legacy"
     ctf     = "WW91IGdvdCBpdCEgbmljZS4="
   }
+
+
+}
+
+resource "aws_eks_addon" "example" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "vpc-cni"
+  resolve_conflicts_on_update = "PRESERVE"
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value     = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+  value     = aws_eks_cluster.eks.certificate_authority[0].data
   sensitive = true
+}
+
+output "oidc-issuer" {
+  value = aws_eks_cluster.eks.identity
 }
 
 
--- infrastructure/scaffold/networking.tf
+++ infrastructure/scaffold/networking.tf
@@ -1,3 +1,9 @@
+locals {
+  eks_availability_zones = toset([
+    "us-east-1a",
+    "us-east-1b",
+  ])
+}
 # Reference https://github.com/hashicorp/learn-terraform-provision-eks-cluster/blob/57e1156420c1f4893c7fc50da8b8dbf322961d34/main.tf#L26
 resource "aws_vpc" "legacy-vpc" {
   cidr_block         = "10.0.0.0/16"
@@ -9,8 +15,8 @@ resource "aws_vpc" "legacy-vpc" {
 # EKS subnets
 # Need to be at least two subnets in two different AZs
 resource "aws_subnet" "eks_subnets" {
-  count             = 2
+  for_each = local.eks_availability_zones
   vpc_id            = aws_vpc.legacy-vpc.id
-  cidr_block        = "10.0.${count.index + 1}.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.${index(tolist(local.eks_availability_zones), each.value) + 1}.0/24"
+  availability_zone = each.value # Ensure multi zones
 }
