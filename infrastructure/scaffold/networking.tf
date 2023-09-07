71a9016915f58c4cf02e21e11b95ec6b4b80a748
Benhur A. Silva <benhur.sre@gmail.com>
2023-09-07 17:58:47 -0300

    feat(eks): create workers node groups
    MTL-18

--- infrastructure/scaffold/networking.tf
+++ infrastructure/scaffold/networking.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "legacy-vpc" {
 # EKS subnets
 # Need to be at least two subnets in two different AZs
 resource "aws_subnet" "eks_subnets" {
-  for_each = local.eks_availability_zones
+  for_each          = local.eks_availability_zones
   vpc_id            = aws_vpc.legacy-vpc.id
   cidr_block        = "10.0.${index(tolist(local.eks_availability_zones), each.value) + 1}.0/24"
   availability_zone = each.value # Ensure multi zones
