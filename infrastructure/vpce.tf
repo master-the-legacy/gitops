07a977d8c334d3c85a5c4dbec61034bb012d1601
carlosgit2016 <carlosggflor@gmail.com>
2023-09-10 01:54:25 -0400

    chore(vpce): Added missing vpc endpoints

--- infrastructure/vpce.tf
+++ infrastructure/vpce.tf
@@ -1,20 +1,29 @@
+locals {
+  vpc_endpoint_names = toset([
+    "ssm",
+    "eks",
+    "ec2messages",
+    "ssmmessages",
+    "ec2",
+  ])
+}
+
 # VPC Endpoints
 # https://docs.aws.amazon.com/whitepapers/latest/aws-privatelink/what-are-vpc-endpoints.html
-resource "aws_vpc_endpoint" "ec2" {
-  vpc_id            = aws_vpc.legacy-vpc.id
+resource "aws_vpc_endpoint" "vpce" {
+  for_each = local.vpc_endpoint_names
   # AWS services that integrate with AWS Private Link
   # https://docs.aws.amazon.com/vpc/latest/privatelink/aws-services-privatelink-support.html
-  service_name      = "com.amazonaws.us-east-1.ec2" ## Allow instances in the same VPC and subnet to communicate with ec2 endpoint, necessary to join EKS cluster
+  service_name      = "com.amazonaws.us-east-1.${each.value}"
   vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.legacy-vpc.id
 
   subnet_ids = [for az in local.eks_availability_zones : aws_subnet.eks_subnets[az].id]
 
   private_dns_enabled = true
 
   tags = {
-    Name = "vpce-ec2"
+    Name = "vpce-${each.value}"
   }
-}
 
-# TODO: Import the rest of the vpc endpoints
-# Consideration for accessing EKS with a VPCe https://docs.aws.amazon.com/eks/latest/userguide/vpc-interface-endpoints.html
+}
