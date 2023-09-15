6f598e26a115968e0a9beb300a48b8841e5b3f4e
carlosgit2016 <carlosggflor@gmail.com>
2023-09-15 00:39:05 -0400

    chore(subnets): removed hardcoded subsnets

--- infrastructure/scaffold/eks-worker-nodes.tf
+++ infrastructure/scaffold/eks-worker-nodes.tf
@@ -7,7 +7,7 @@ resource "aws_eks_node_group" "node-group-0" {
 
   # the for_each meta-arg created a map with two key-values pairs: {"us-east-1a" = {attributes of subnet-a(key:value)}, "us-east-1b" = {attributes of subnet-b(key:value)}}
   # values function get a map and return a list with the values of that map. [{attributes of subnet-a(key:value)}, {attributes of subnet-b(key:value)}]
-  subnet_ids = [local.subnet_ids["us-east-1a"]]
+  subnet_ids = [data.aws_subnets.eks_subnets.ids[1]]
 
   #subnet_ids      = [key(aws_subnet.eks_subnets)[0].id]       # using key function would work also (not tested)                                              
   capacity_type  = "SPOT"
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "node-group-1" {
 
   # the for_each meta-arg created a map with two key-values pairs: {"us-east-1a" = {attributes of subnet-a(key:value)}, "us-east-1b" = {attributes of subnet-b(key:value)}}
   # values function get a map and return a list with the values of that map. [{attributes of subnet-a(key:value)}, {attributes of subnet-b(key:value)}]
-  subnet_ids = [local.subnet_ids["us-east-1b"]]
+  subnet_ids = [data.aws_subnets.eks_subnets.ids[0]]
 
   #subnet_ids      = [key(aws_subnet.eks_subnets)[1].id]       # using key function would work also (not tested)
   capacity_type  = "SPOT"
